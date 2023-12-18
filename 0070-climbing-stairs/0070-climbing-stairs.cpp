56f94eef648a65c2586fbd222a6947fb37a5460b
Ayush Mohan Pandey <96104562+AyushMohanPandey@users.noreply.github.com>
2023-12-18 23:56:53 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (33.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -8,20 +8,37 @@
 //     }
 // };
 
-//******* Memoization **********         TC = O(N), SC = O(N)+O(N)
+// ******* Memoization **********         TC = O(N), SC = O(N)+O(N)
+// class Solution {
+// public:
+//     int dpSolve(int n, vector<int> &dp)
+//     {
+//         if(n==0 || n==1)
+//             return 1;
+//         if(dp[n] != -1)
+//             return dp[n];
+//         dp[n] = dpSolve(n-1,dp) + dpSolve(n-2,dp);
+//         return dp[n];
+//     }
+//     int climbStairs(int n) {
+//         vector<int> dp(n+1,-1);
+//         return dpSolve(n,dp);
+//     }
+// };
+
+// ******* Tabulation **********         TC = O(N), SC = O(N)+O(N)
 class Solution {
 public:
-    int dpSolve(int n, vector<int> &dp)
-    {
+    int climbStairs(int n) {
         if(n==0 || n==1)
             return 1;
-        if(dp[n] != -1)
-            return dp[n];
-        dp[n] = dpSolve(n-1,dp) + dpSolve(n-2,dp);
-        return dp[n];
-    }
-    int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        return dpSolve(n,dp);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
