b7921812963f9c637525d33494fc32523f2851a9
Ayush Mohan Pandey <96104562+AyushMohanPandey@users.noreply.github.com>
2023-12-19 00:00:29 +0530

    Time: 2 ms (45.60%), Space: 6.3 MB (70.27%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -26,19 +26,38 @@
 //     }
 // };
 
-// ******* Tabulation **********         TC = O(N), SC = O(N)+O(N)
+// ******* Tabulation **********         TC = O(N), SC = O(N)
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if(n==0 || n==1)
+//             return 1;
+//         vector<int> dp(n+1,-1);
+//         dp[0] = 1;
+//         dp[1] = 1;
+//         for(int i=2;i<=n;i++)
+//         {
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// };
+
+// ******** Space optimization *******   TC = O(N), SC = O(1); 
+
 class Solution {
 public:
     int climbStairs(int n) {
         if(n==0 || n==1)
             return 1;
-        vector<int> dp(n+1,-1);
-        dp[0] = 1;
-        dp[1] = 1;
+        int prev2 = 1;
+        int prev = 1;
         for(int i=2;i<=n;i++)
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
         }
-        return dp[n];
+        return prev;
     }
 };
