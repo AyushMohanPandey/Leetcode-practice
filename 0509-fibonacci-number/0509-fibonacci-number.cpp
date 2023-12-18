c2358a2bf84c3858338dccb4a614a7097dfdb0c2
Ayush Mohan Pandey <96104562+AyushMohanPandey@users.noreply.github.com>
2023-12-18 22:46:45 +0530

    Time: 2 ms (75.15%), Space: 6.5 MB (25.01%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,4 +1,4 @@
-// ******* Recurion approach ********
+// ******* Recurion approach ********       TC = O(2^N) , SC = O(N)
 
 // class Solution {
 // public:
@@ -9,23 +9,48 @@
 //     }
 // };
 
-// ******* Memoization approach *******
+// ******* Memoization approach *******     TC = O(N) , SC = O(N)+O(N)
+
+// class Solution {
+//     public:
+//     int dpSolve(int n, vector<int> &dp)
+//     {
+//         if(n<=1)
+//             return n;
+//         if(dp[n] != -1)
+//             return dp[n];
+//         dp[n] = dpSolve(n-1, dp) + dpSolve(n-2, dp);
+//         return dp[n];
+//     }
+    
+//     int fib(int n) {
+//         vector<int> dp(n+1,-1);
+//         // dpSolve(n, dp);
+//         return dpSolve(n, dp);
+//     }
+// };
+
+// ******** Tabulation ********        TC = O(N) , SC = O(N)
 
 class Solution {
-    public:
-    int dpSolve(int n, vector<int> &dp)
-    {
+public:
+    int fib(int n) {
         if(n<=1)
             return n;
-        if(dp[n] != -1)
-            return dp[n];
-        dp[n] = dpSolve(n-1, dp) + dpSolve(n-2, dp);
+        vector<int> dp(n+1, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         return dp[n];
     }
-    
-    int fib(int n) {
-        vector<int> dp(n+1,-1);
-        // dpSolve(n, dp);
-        return dpSolve(n, dp);
-    }
 };
+
+
+
+
+
+
+
