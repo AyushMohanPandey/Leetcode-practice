c4db54af3f7cac42c179ba1aab9df984b0b813b9
Ayush Mohan Pandey <96104562+AyushMohanPandey@users.noreply.github.com>
2023-12-25 03:03:34 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (10.74%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -10,21 +10,41 @@
 // };
 
 
+// class Solution {
+// public:
+//     int dpSolve(int n, vector<int> &dp)
+//     {
+//         if(n==0)
+//              return 0;
+//         if(n==1 || n==2)
+//              return 1;
+//         if(dp[n] != -1)
+//             return dp[n];
+//         dp[n] = dpSolve(n-1, dp)+dpSolve(n-2, dp)+dpSolve(n-3, dp);
+//         return dp[n];
+//     }
+//     int tribonacci(int n) {
+//         vector<int> dp(n+1,-1);
+//         return dpSolve(n, dp);
+//     }
+// };
+
+
 class Solution {
 public:
-    int dpSolve(int n, vector<int> &dp)
-    {
+    int tribonacci(int n) {
+        vector<int> dp(n+1,-1);
         if(n==0)
              return 0;
         if(n==1 || n==2)
              return 1;
-        if(dp[n] != -1)
-            return dp[n];
-        dp[n] = dpSolve(n-1, dp)+dpSolve(n-2, dp)+dpSolve(n-3, dp);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i=3;i<=n;i++)
+        {
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3]; 
+        }
         return dp[n];
-    }
-    int tribonacci(int n) {
-        vector<int> dp(n+1,-1);
-        return dpSolve(n, dp);
     }
 };
