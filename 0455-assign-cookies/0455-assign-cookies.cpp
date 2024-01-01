bc5657032f2f51a452893960f38aa817cfa42583
Ayush Mohan Pandey <96104562+AyushMohanPandey@users.noreply.github.com>
2024-01-01 15:19:22 +0530

    Time: 61 ms (6.00%), Space: 17.8 MB (36.11%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
+    // int findContentChildren(vector<int>& g, vector<int>& s) {
+    //     int c=0;
+    //     sort(g.begin(), g.end());
+    //     sort(s.begin(), s.end());
+    //     for(int i=0;i<g.size();i++)
+    //     {
+    //         for(int j=0;j<s.size();j++)
+    //         {
+    //             if(g[i]<=s[j])
+    //             {
+    //                 s[j]=-1;
+    //                 c++;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return c;
+    // }
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int c=0;
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
-        for(int i=0;i<g.size();i++)
-        {
-            for(int j=0;j<s.size();j++)
-            {
-                if(g[i]<=s[j])
-                {
-                    s[j]=-1;
-                    c++;
-                    break;
-                }
-            }
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i = 0, j=0;
+        while(i<g.size() && j<s.size()){
+            if(s[j]>=g[i])
+                i++; // when the child get the cookie, foward child by 1
+            j++;
         }
-        return c;
+        return i;
     }
 };
