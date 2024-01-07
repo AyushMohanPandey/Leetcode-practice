3344707da2af288535ac66f0e845d0fd5b2ae099
Ayush Mohan Pandey <96104562+AyushMohanPandey@users.noreply.github.com>
2024-01-08 01:38:45 +0530

    Time: 86 ms (79.44%), Space: 13 MB (99.44%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
+    // int numberOfArithmeticSlices(vector<int>& A, int res = 0) {
+    // unordered_map<long, int> m[max(1, (int)A.size())];
+    // for (int i = 0; i < A.size(); ++i)
+    //     for (int j = 0; j < i; ++j) {
+    //         auto it = m[j].find((long)A[i] - A[j]);
+    //         int cnt = it == end(m[j]) ? 0 : it->second;
+    //         m[i][(long)A[i] - A[j]] += cnt + 1;
+    //         res += cnt;
+    //     }
+    // return res;
+    // }
+    
     int numberOfArithmeticSlices(vector<int>& A, int res = 0) {
-    unordered_map<long, int> m[max(1, (int)A.size())];
+    unordered_map<int, int> m[max(1, (int)A.size())];
+    unordered_set<int> s(begin(A), end(A));
     for (int i = 0; i < A.size(); ++i)
         for (int j = 0; j < i; ++j) {
-            auto it = m[j].find((long)A[i] - A[j]);
+            long d = (long)A[i] - A[j];
+            if (d < INT_MIN || d > INT_MAX)
+                continue;
+            auto it = m[j].find(d);
             int cnt = it == end(m[j]) ? 0 : it->second;
-            m[i][(long)A[i] - A[j]] += cnt + 1;
+            if (s.count((long)A[i] + d))
+                m[i][d] += cnt + 1;
             res += cnt;
         }
     return res;
