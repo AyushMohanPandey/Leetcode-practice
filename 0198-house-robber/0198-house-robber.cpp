022b4f1adbcecf8bf34eeb2ebe4d00629f474e74
Ayush Mohan Pandey <96104562+AyushMohanPandey@users.noreply.github.com>
2024-01-21 20:17:52 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -24,14 +24,30 @@ public:
     
     //Approach 3    Time Complexity : O(n)    Space Complexity : O(n)
     
+    // int rob(vector<int>& nums) {
+    //     if(nums.size()==1)return nums[0];
+    //     vector<int>dp(nums.size());
+    //     dp[0]=nums[0];
+    //     dp[1]=max(nums[0],nums[1]);
+    //     for(int i=2;i<nums.size();i++){
+    //         dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+    //     }
+    //     return dp[nums.size()-1];
+    // }
+    
+    //Approach 4    Time Complexity : O(n)    Space complexity : O(1)
+    
     int rob(vector<int>& nums) {
-        if(nums.size()==1)return nums[0];
-        vector<int>dp(nums.size());
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        for(int i=2;i<nums.size();i++){
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        
+        int prev_ans2=nums[0], prev_ans=max(nums[0],nums[1]),curr_ans=prev_ans;
+        
+        for(int i = 2; i < n; i++){
+            curr_ans = max(prev_ans, prev_ans2 + nums[i]);
+            prev_ans2 = prev_ans;
+            prev_ans = curr_ans;
         }
-        return dp[nums.size()-1];
+        return curr_ans;
     }
 };
