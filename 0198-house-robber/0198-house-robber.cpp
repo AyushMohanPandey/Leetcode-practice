443a2d52c9ce87fbfe4eb1051aa002d98827cb71
Ayush Mohan Pandey <96104562+AyushMohanPandey@users.noreply.github.com>
2024-01-21 20:13:23 +0530

    Time: 4 ms (25.45%), Space: 9.1 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    //Approach 1
+    //Approach 1    Time Complexcity : O ( 2^n ) Gives us TLE   Space Complexcity : O( 1 )
+    
     // int rec(vector<int>& nums,int i){
     //     if(i>=nums.size())return 0;
     //     return max(nums[i]+rec(nums,i+2),rec(nums,i+1));
@@ -9,14 +10,28 @@ public:
     //     return rec(nums,0);
     // }
     
-    //Approach 2
-    int rec(vector<int>& nums,int idx,vector<int>&dp){
-       if(idx >= nums.size()) return 0;
-       if(dp[idx] != -1) return dp[idx];
-       return dp[idx] = max(rec(nums, idx+1, dp), nums[idx] + rec(nums, idx+2, dp));
-     }
+    //Approach 2    Time Complexcity : O (n)    Space Complexcity : O(n)
+    
+    // int rec(vector<int>& nums,int idx,vector<int>&dp){
+    //    if(idx >= nums.size()) return 0;
+    //    if(dp[idx] != -1) return dp[idx];
+    //    return dp[idx] = max(rec(nums, idx+1, dp), nums[idx] + rec(nums, idx+2, dp));
+    //  }
+    // int rob(vector<int>& nums) {
+    //   vector<int>dp(nums.size()+1,-1);
+    //   return rec(nums,0,dp);
+    // }
+    
+    //Approach 3    Time Complexity : O(n)    Space Complexity : O(n)
+    
     int rob(vector<int>& nums) {
-      vector<int>dp(nums.size()+1,-1);
-      return rec(nums,0,dp);
+        if(nums.size()==1)return nums[0];
+        vector<int>dp(nums.size());
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<nums.size();i++){
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[nums.size()-1];
     }
 };
