e0b34225a879e1c70e55123a6d36e85f89eb0eac
Ayush Mohan Pandey <96104562+AyushMohanPandey@users.noreply.github.com>
2024-01-24 00:52:08 +0530

    Time: 51 ms (56.57%), Space: 34.2 MB (57.81%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,29 +1,69 @@
+// class Solution {
+// public:
+//     bool check(string s,unordered_map<char,int> &map){
+//         unordered_set<char> mp;
+//         for(auto it:s){
+//             if(mp.find(it)!=mp.end())return false;//found duplicate in the same string
+//                 mp.insert(it);
+//             if(map[it]>0)return false;//found duplicate in the map and current string
+//         }
+//         return true;
+//     }
+//     int solve(int i,vector<string> &arr,unordered_map<char,int> &map){
+//         if(i>=arr.size())return 0;
+//         int x=0;
+//         x=solve(i+1,arr,map);//not pick
+//         if(check(arr[i],map)){ //checking that it is possible to pick or not
+//             for(auto it:arr[i])map[it]++; //putting in our map
+//             x=max(x,(int)(solve(i+1,arr,map)+arr[i].size()));
+//             for(auto it:arr[i]){//backtracking
+//                 map[it]--;
+//             }
+//         }
+//         return x;
+//     }
+//     int maxLength(vector<string>& arr) {
+//         unordered_map<char,int> map;
+//         return solve(0,arr,map);
+//     }
+// };
+
 class Solution {
 public:
-    bool check(string s,unordered_map<char,int> &map){
-        unordered_set<char> mp;
-        for(auto it:s){
-            if(mp.find(it)!=mp.end())return false;//found duplicate in the same string
-                mp.insert(it);
-            if(map[it]>0)return false;//found duplicate in the map and current string
-        }
-        return true;
+    int maxLength(vector<string>& arr) {
+        int maxLength = 0;
+        backTrack(arr, "", 0, maxLength);
+        return maxLength;
     }
-    int solve(int i,vector<string> &arr,unordered_map<char,int> &map){
-        if(i>=arr.size())return 0;
-        int x=0;
-        x=solve(i+1,arr,map);//not pick
-        if(check(arr[i],map)){ //checking that it is possible to pick or not
-            for(auto it:arr[i])map[it]++; //putting in our map
-            x=max(x,(int)(solve(i+1,arr,map)+arr[i].size()));
-            for(auto it:arr[i]){//backtracking
-                map[it]--;
+
+private:
+    void backTrack(const vector<string>& arr, string current, int start, int& maxLength) {
+        if (maxLength < current.length())
+            maxLength = current.length();
+
+        for (int i = start; i < arr.size(); i++) {
+            if (!isValid(current, arr[i]))
+                continue;
+
+            backTrack(arr, current + arr[i], i + 1, maxLength);
+        }
+    }
+
+    bool isValid(const string& currentString, const string& newString) {
+        unordered_set<char> charSet;
+
+        for (char ch : newString) {
+            if (charSet.count(ch) > 0) {
+                return false; 
+            }
+
+            charSet.insert(ch);
+
+            if (currentString.find(ch) != string::npos) {
+                return false;  
             }
         }
-        return x;
-    }
-    int maxLength(vector<string>& arr) {
-        unordered_map<char,int> map;
-        return solve(0,arr,map);
+
+        return true;
     }
 };
